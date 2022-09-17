c213172244fbeb8baf54c48a2f84b4d3a9e9601f
Andrey Soktoev <andrey.soktoev@gmail.com>
2022-09-18 04:38:41 +0800

    URLSearchParams

--- hh/app.js
+++ hh/app.js
@@ -1,22 +1,34 @@
 import { fetch } from 'undici'
 
-async function run(page) {
-  // const url = 'https://api.hh.ru/vacancies?text=node+and+not+senior&search_field=name&schedule=remote&experience=noExperience&experience=between1And3&period=30&vacancy_search_order=publication_time&responses_count_enabled=true'
-  // const url = 'https://api.hh.ru/vacancies?specialization=1.221&text=junior&search_field=name&period=30&vacancy_search_order=publication_time&responses_count_enabled=true&per_page=100&page=7'
-  const url = `https://api.hh.ru/vacancies?professional_role=96&text=node.js+or+nodejs+or+node&search_field=name&period=30&per_page=100&page=${page}`
-  const res = await fetch(url)
-  const json = await res.json()
-  // const vacancies = json.items.map(i => ({
-  //   employer: i.employer.name,
-  //   title: i.name,
-  //   salary: i.salary,
-  //   requirements: i.snippet.requirement,
-  //   url: i.alternate_url,
-  //   responses: i.counters.responses
-  // }))
-  const vacancies = json.items.map(i => i.name)
-  console.log(vacancies)
-  // console.log(vacancies.length)
-}
+(async () => {
+  const PAGES = 5
+  const url = new URL('https://api.hh.ru/vacancies')
+  const params = new URLSearchParams({
+    per_page: '100',
+    period: '7',
+    professional_role: '96',
+    responses_count_enabled: true,
+    search_field: 'name',
+    text: '(node.js or nodejs or node) and not senior',
+    vacancy_search_order: 'publication_time',
+  })
+  url.search = params
+  let result = []
 
-for (let i = 0; i < 4; i++) run(i)
+  for (let i = 0; i < PAGES; i++) {
+    params.set('page', i)
+    url.search = params
+    const res = await fetch(url)
+    const json = await res.json()
+    json.items.map(i => {
+      if (i) result.push({
+        id: i.id,
+        employer: i.employer.name,
+        title: i.name,
+        responses: i.counters.responses
+      })
+    })
+  }
+
+  console.table(result)
+})()
